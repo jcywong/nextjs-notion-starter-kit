96f30ca66e7428869b5879c7a7d913df999aed1a
Jcy <jcywong@qq.com>
2022-05-18 20:21:04 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Jcy’s Blog',
+  domain: 'https://notion.jcywong.top',
+  author: 'Jcy Wong',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Jcy’s Blog',
 
   // social usernames (optional)
   //twitter: 'transitive_bs',
